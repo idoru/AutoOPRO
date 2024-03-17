aa714425adb3190edb5797182ced94a1a78ee588
Zain ul Abideen <zaiinn440@gmail.com>
2024-03-17 13:13:37 +0500

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -1,16 +1,16 @@
 pip install datasets accelerate wandb transformers bitsandbytes
 git clone https://github.com/xfactlab/orpo.git
-!wandb login $WANDB_TOKEN
-!wandb init -p $MODEL_ID
 cd orpo
 sed -i 's/num_processes: 2/num_processes: 1/' ./src/accelerate/fsdp.yaml
 sed -i 's/--num_proc", default=8/--num_proc", default=1/' ./src/args.py
+!wandb login $WANDB_TOKEN
+!wandb init -p $MODEL_ID
 accelerate launch --config_file ./src/accelerate/fsdp.yaml main.py \
     --lr $LEARNING_RATE \
     --warmup_steps 100 \
     --model_name $MODEL_ID \
     --data_name $DATASET \
-    --num_train_epochs $EPOCHS \
+    --num_train_epochs $EPOCH \
     --prompt_max_length 128 \
     --response_max_length 2048 \
     --per_device_train_batch_size 4 \
