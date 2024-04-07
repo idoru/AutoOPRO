007690c5de994d6cb0b10b586c13076e9c096924
Sam Coward <idoru42@gmail.com>
2024-04-07 15:56:29 -0400

    oops. pass num_proc properly. give fsdp another chance

--- runpod.sh
+++ runpod.sh
@@ -5,14 +5,14 @@ git clone https://github.com/xfactlab/orpo.git
 cd orpo
 sed -i 's/num_processes: 2/num_processes: 4/' ./src/accelerate/fsdp.yaml
 sed -i 's/num_processes: 2/num_processes: 4/' ./src/accelerate/ds2.yaml
-sed -i 's/gradient_accumulation_steps: 1/gradient_accumulation_steps: 4/' ./src/accelerate/ds2.yaml
+sed -i 's/gradient_accumulation_steps: 1/gradient_accumulation_steps: 8/' ./src/accelerate/ds2.yaml
 sed -i 's/--num_proc", default=8/--num_proc", default=4/' ./src/args.py
 sed -i 's/gradient_checkpointing=True/gradient_checkpointing=False/' ./main.py
 
 
 wandb login $WANDB_TOKEN
 wandb init -p $WANDB_PROJECT
-accelerate launch --config_file ./src/accelerate/ds2.yaml main.py \
+accelerate launch --config_file ./src/accelerate/fsdp.yaml main.py \
     --lr $LEARNING_RATE \
     --warmup_steps 100 \
     --model_name $MODEL_ID \
@@ -21,9 +21,9 @@ accelerate launch --config_file ./src/accelerate/ds2.yaml main.py \
     --prompt_max_length 128 \
     --response_max_length 2048 \
     --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
-    --num_proc 1
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --num_proc 4
 cd $OUTPUT
 cd */
 huggingface-cli login --token $TOKEN
