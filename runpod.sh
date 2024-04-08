d495e59d5f3bb2374b031424f51838274f6dff1a
Sam Coward <idoru42@gmail.com>
2024-04-07 22:17:42 -0400

    debugging stuff

--- runpod.sh
+++ runpod.sh
@@ -5,6 +5,12 @@ GRAD_ACC_STEPS=8
 pip install datasets accelerate deepspeed wandb git+https://github.com/yikangshen/transformers.git bitsandbytes sentencepiece peft
 git clone https://github.com/xfactlab/orpo.git
 cd orpo
+
+wandb login $WANDB_TOKEN
+wandb init -p $WANDB_PROJECT
+
+set -x
+
 sed -i 's/num_processes: 2/num_processes: '${NUM_GPUS}'/' ./src/accelerate/fsdp.yaml
 sed -i 's/num_processes: 2/num_processes: '${NUM_GPUS}'/' ./src/accelerate/ds2.yaml
 sed -i 's/gradient_accumulation_steps: 1/gradient_accumulation_steps: '${GRAD_ACC_STEPS}'/' ./src/accelerate/ds2.yaml
@@ -13,9 +19,6 @@ sed -i 's/--num_proc", default=8/--num_proc", default='${NUM_GPUS}'/' ./src/args
 
 #Seems like JetMoE doesnt support this
 #sed -i 's/gradient_checkpointing=True/gradient_checkpointing=False/' ./main.py
-
-wandb login $WANDB_TOKEN
-wandb init -p $WANDB_PROJECT
 
 accelerate launch --config_file ./src/accelerate/fsdp.yaml main.py \
     --lr $LEARNING_RATE \
@@ -29,6 +32,7 @@ accelerate launch --config_file ./src/accelerate/fsdp.yaml main.py \
     --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps $GRAD_ACC_STEPS \
     --num_proc $NUM_GPUS
+set +x
 
 cd $OUTPUT
 cd */
